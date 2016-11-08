f8c22a3c5b299a5113ace7f550d0d8d1305f63f0
Rachel Platz <Rachel Platz>
2016-11-08 11:15:50 -0800

    Patterns3

---
+++ unity_project/Assets/scripts/RP_Switch.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RP_Switch : MonoBehaviour {
+
+	public GameObject lever;
+
+	private bool MoveLever = true;
+
+	/*void Update(){
+		if (Input.GetMouseButtonDown (0) && MoveLever == false) {
+			MoveLever = true;
+		} else if (Input.GetMouseButtonDown (0) && MoveLever== true) {
+			MoveLever = false;
+		}
+	}
+
+	void FixedUpdate(){
+		Rigidbody rb = lever.GetComponent<Rigidbody> ();
+
+
+		if (MoveLever) {
+			RotateLever ();
+		}
+		if(!MoveLever){
+			RotateLever ();
+		}
+	}
+
+	void RotateLever(){
+		float x = lever.transform.rotation.x;
+		float y = lever.transform.rotation.y;
+		float z = lever.transform.rotation.z;
+		Vector3 move = new Vector3 (x + 90.0f, y, z);
+		lever.transform.rotation = Quaternion.Euler (move);
+	} // */
+
+	void OnMouseDown(){
+		float x = lever.transform.rotation.x;
+		float y = lever.transform.rotation.y;
+		float z = lever.transform.rotation.z;
+		if (MoveLever) {
+			Vector3 move = new Vector3 (90.0f, y, z);
+			lever.transform.rotation = Quaternion.Euler (move);
+			MoveLever = false;
+		} else if (!MoveLever) {
+			Vector3 move = new Vector3 (0.0f, y, z);
+			lever.transform.rotation = Quaternion.Euler (move);
+			MoveLever = true;
+		}
+	}
+}
